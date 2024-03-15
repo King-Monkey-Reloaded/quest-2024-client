f7991f7f8419cd25651e2ba10f28a997cb8f17d7
chikipack <victorgotzilla@hotmail.com>
2024-03-14 23:38:26 -0700

    route to welcome component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { DiscordloginComponent } from './dummycomps/discordlogin/discordlogin.component';
-import { LoginComponent } from './dummycomps/login/login.component';
 import { Test1Component } from './dummycomps/test1/test1.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 export const routes: Routes = [
-    {path:'login', component: LoginComponent},
+    {path: 'welcome', component: WelcomeComponent}, 
     {path:'auth/discord/login', component: DiscordloginComponent},
     {path:'api/auth/discord/redirect', component: Test1Component},
-    {path:'', redirectTo:'/login', pathMatch:'full'},
+    {path:'', redirectTo:'/welcome', pathMatch:'full'},
 ];
