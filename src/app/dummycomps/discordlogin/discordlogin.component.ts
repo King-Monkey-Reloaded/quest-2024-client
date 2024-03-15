e1611edccac5b83a8f4af79d0a8e9a7b1d2bbd41
chikipack <victorgotzilla@hotmail.com>
2024-03-14 18:45:28 -0700

    calling the apis

--- src/app/dummycomps/discordlogin/discordlogin.component.ts
+++ src/app/dummycomps/discordlogin/discordlogin.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class DiscordloginComponent {
   ngOnInit(): void {
     // Redirige al usuario a la URL de autorización de Discord
-    const url = 'https://discord.com/oauth2/authorize?client_id=1216916993106645003&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fapi%2Fauth%2Fdiscord%2Fredirect&scope=identify+guilds+guilds.members.read';
+    const url = 'https://discord.com/oauth2/authorize?client_id=1216916993106645003&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fapi%2Fauth%2Fdiscord%2Fredirect&scope=identify+guilds.members.read+guilds';
     window.location.href = url;
   }
 }
