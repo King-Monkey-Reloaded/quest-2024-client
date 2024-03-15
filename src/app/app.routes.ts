3255a6a5e3ee8c162f2db7ab1cd33b985886febe
chikipack <victorgotzilla@hotmail.com>
2024-03-15 13:37:40 -0700

    routing to -admin panel -Home user - redirect component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
-import { DiscordloginComponent } from './dummycomps/discordlogin/discordlogin.component';
-import { Test1Component } from './dummycomps/test1/test1.component';
+import { DiscordloginComponent } from '@pages/discordlogin/discordlogin.component';
+// import { Test1Component } from './dummycomps/test1/test1.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { RedirectComponent } from '@shared/components/redirect/redirect.component';
+import { UserHomeComponent } from '@pages/user/user-home/user-home.component';
+import { AdminComponent } from '@pages/admin/admin.component';
 
 export const routes: Routes = [
     {path: 'welcome', component: WelcomeComponent}, 
     {path:'auth/discord/login', component: DiscordloginComponent},
-    {path:'api/auth/discord/redirect', component: Test1Component},
+    {path:'api/auth/discord/redirect', component: RedirectComponent},
+    {path:'home', component: UserHomeComponent},
+    {path: 'panel', component: AdminComponent},
     {path:'', redirectTo:'/welcome', pathMatch:'full'},
 ];
